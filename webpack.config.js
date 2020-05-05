9ff6e4bdc8977ec0eabdcb5cd3cbfb0682f4d6e2
David Pacheco <davidpacheco89@gmail.com>
2020-05-04 19:51:13 -0500

    eslint implemented

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    { loader: 'css-loader', options: { importLoaders: 1 } },
-                    'postcss-loader'
-                ]
-            }
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
         ]
-    }
+      }
+    ]
+  }
 }
