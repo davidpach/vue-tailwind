a644d370ce4d35cdaef99b20a1dbd23235444c9b
David Pacheco <davidpacheco89@gmail.com>
2021-01-18 13:10:19 -0600

    Added vuex, router

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,5 @@
 <template>
-  <div id="app">
-    <div class="flex flex-col justify-center items-center">
-      <div class="m-auto mb-10">
-        <img
-          alt="Vue logo"
-          class
-          src="./assets/logo.png"
-        >
-      </div>
-      <div class="font-extrabold">
-        vue tailwind | dpo
-      </div>
-    </div>
+  <div class="max-w-lg m-auto">
+    <RouterView :key="$route.fullPath" />
   </div>
 </template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home flex flex-col justify-center items-center h-screen">
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+      class="w-20"
+    >
+    <div class="w-full text-center">
+      Vue - Vuex - Router - Tailwind
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
